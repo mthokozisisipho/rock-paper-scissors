f307da31304c80edd5c7c5428e79ba1265c4cfbe
mthokozisisipho <122373617+mthokozisisipho@users.noreply.github.com>
2023-06-12 19:43:22 +0200

    Add function that plays single round of the game

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ function getComputerChoice() {
         return ("scissors");
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        console.log("It's a Tie!");
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("You won! Rock beats Scissors");
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("You won! Paper beats Rock");
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("You won! Scissors beats Paper");
+    }
+    else {
+        console.log(`You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`);
+    }
+}
