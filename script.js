290951117992823ef87afba90779f2f5a4969cc8
mthokozisisipho <122373617+mthokozisisipho@users.noreply.github.com>
2023-06-12 19:50:44 +0200

    Let playRound return instead

--- script.js
+++ script.js
@@ -17,18 +17,18 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
-        console.log("It's a Tie!");
+        return ("It's a Tie!");
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("You won! Rock beats Scissors");
+        return ("You won! Rock beats Scissors");
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You won! Paper beats Rock");
+        return ("You won! Paper beats Rock");
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You won! Scissors beats Paper");
+        return ("You won! Scissors beats Paper");
     }
     else {
-        console.log(`You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`);
+        return (`You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`);
     }
 }
